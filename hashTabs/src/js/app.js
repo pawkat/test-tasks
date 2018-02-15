87ba05a05490a7e2dd0b8a0956240dbedb73fe6e
pawkat <p.p.pawkat@gmail.com>
2018-02-15 18:19:13 +0200

    edits

--- hashTabs/src/js/app.js
+++ hashTabs/src/js/app.js
@@ -3,74 +3,50 @@ import sayHello from './lib/sayHello.js';
 sayHello();
 
 
-// var content = $('.content');
 // var loader = document.createElement('div');
 // $(loader).addClass('loader');
-// $('.header__link').on('click', function(e) {
-//   var $this = $(this);
-//   $('.header__link').removeClass('active');
-//   $this.addClass('active');
-//   $this.addClass('active');
-//   $.ajax({
-//     url: $this.attr('href'),
-//     dataType: 'html',
-//     context: content.html(loader),
-//     success: function(html) {
-//       loader.remove();
-//       this.html(html).fadeIn('fast');
-//       console.log(html);
-//     }
-//   });
-//   window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
-//   e.preventDefault();
-//   return false;
+// $.ajax({
+//   url: $this.attr('href'),
+//   dataType: 'html',
+//   context: container.html(loader),
+//   success: function(html) {
+//     loader.remove();
+//     this.html(html).fadeIn('fast');
+//   }
 // });
 
+function hashTabs(link, sections, showClass, linkActiveClass) {
 
 
 
+  link.on('click', function() {
+    $(link).removeClass(linkActiveClass);
+    $(this).addClass(linkActiveClass);
+    var index = $(this).index();
+    var section = sections[index];
+    sections.removeClass(showClass);
+    $(section).addClass(showClass);
 
-function hashTabs(container, linkClass) {
-  var container = $(container);
-  var link = $(linkClass);
-  var loader = document.createElement('div');
-  $(loader).addClass('loader');
-  $(link).on('click', function() {
-    var $this = $(this);
-    $(link).removeClass('active');
-    $this.addClass('active');
-    $.ajax({
-      url: $this.attr('href'),
-      dataType: 'html',
-      context: container.html(loader),
-      success: function(html) {
-        loader.remove();
-        this.html(html).fadeIn('fast');
-      }
-    });
-    window.location.hash = $(this).attr('href').substr(0, $(this).attr('href').length - 5);
+
+
+    window.location.hash = $(this).attr('href');
     return false;
   });
+  setInterval(function page() {
+    // var findLink = 'a' + '[href="' + window.location.hash.substr(1, window.location.hash.length) + '"]';
+    var hash = window.location.hash.substr(1, window.location.hash.length);
+    var findLink ='a' + '[href="' + hash + '"]';
+    var link = $(findLink);
+    link.parent().children().removeClass(linkActiveClass);
+    link.addClass(linkActiveClass);
+    var sectionClass = '.' + hash;
+    var section = $(sectionClass);
+    section.parent().children().removeClass(showClass);
+    section.addClass(showClass);
+  }, 1000);
 }
-hashTabs(($('.content')), ($('.header__link')));
-hashTabs(($('.content-2')), ($('.header__link2')));
+hashTabs($('.header__link'), $('.block'), 'block_show', 'active');
+hashTabs($('.header__link2'), $('.block2'), 'block_show', 'active');
 
 
-setInterval(function page() {
-  var desiredLinkClass = '.' + window.location.hash.substr(1, 5);
-  var desiredLink = $(desiredLinkClass);
-  var url = desiredLink.attr('href');
-  if (desiredLink.hasClass('active') === false) {
-    $('.header-link').removeClass('active');
-    desiredLink.addClass('active');
-    $.ajax({
-      url: url,
-      dataType: 'html',
-      context: content.html(loader),
-      success: function(html) {
-        loader.remove();
-        this.html(html).fadeIn('fast');
-      }
-    });
-  }
-}, 1000);
+
